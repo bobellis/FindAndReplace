cb2b795fe527f898ba5b07c9b2f624ec6f7ff057
Bob Ellis <bobellis.tx@gmail.com>
2015-05-29 11:43:03 -0700

    updated styling, set result to jumbotron

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,16 @@ var findAndReplace = function(textToReplace, replacementText, message){
   }
     return wordArray.join(" ")
 };
+
+$(function(){
+  $("form#message-form").submit(function(event) {
+    var message = $("#message").val();
+    var textToReplace = $("#textToReplace").val();
+    var replacementText = $("#replacementText").val();
+    $("#replaced-message").text('Your new string: ' + findAndReplace(textToReplace, replacementText, message));
+
+    $("#replaced-message").show()
+    event.preventDefault();
+  });
+
+})
