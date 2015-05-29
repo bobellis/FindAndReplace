d18a70a5d7d7cc983ea36856f67d0b4f710427bd
Bob Ellis <bobellis.tx@gmail.com>
2015-05-29 12:12:23 -0700

    Add new spec to test for multiple occurrences of word

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var findAndReplace = function(textToReplace, replacementText, message){
   var wordArray = message.split(" ")
   for(var i = 0; i <= wordArray.length; i++){
+    
     if(wordArray[i] === textToReplace){
       wordArray[i] = replacementText;
     };
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,15 @@
 describe('findAndReplace', function() {
-  it('accepts a string and two words and returns the string with the word apple replaced by the word pumpkin', function() {
+  it('accepts a string and two words and returns the string with the word apple replaced by the predefined word pumpkin', function() {
       expect(findAndReplace("apple", "pumpkin", "Let's have some apple pie.")).to.equal("Let's have some pumpkin pie.");
   });
 
   it('accepts a string and two words and returns the string with the word replaced by the second word', function(){
     expect(findAndReplace("pumpkin", "apple", "Let's have some pumpkin pie.")).to.equal("Let's have some apple pie.");
   });
+
+
+  it('accepts a string and two words and returns the string with all instances of the word replaced by the second word', function(){
+    expect(findAndReplace("pumpkin", "apple", "Hey pumpkin let's have some pumpkin pie.")).to.equal("Hey apple let's have some apple pie.");
+  });
+
 });
