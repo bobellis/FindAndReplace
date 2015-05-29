9c0e53d4957a25baa78e7d2abb3cc62e4648e11d
Bob Ellis <bobellis.tx@gmail.com>
2015-05-29 10:37:50 -0700

    add spec to test findreplace method using user defined words and code to pass

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('findAndReplace', function() {
   it('accepts a string and two words and returns the string with the word apple replaced by the word pumpkin', function() {
       expect(findAndReplace("apple", "pumpkin", "Let's have some apple pie.")).to.equal("Let's have some pumpkin pie.");
   });
+
+  it('accepts a string and two words and returns the string with the word replaced by the second word', function(){
+    expect(findAndReplace("pumpkin", "apple", "Let's have some pumpkin pie.")).to.equal("Let's have some apple pie.");
+  });
 });
